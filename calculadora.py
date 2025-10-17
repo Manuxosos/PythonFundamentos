2170a91f4b4855ae0e4626d2d02c16e50d5bb4ff
Manuxosos <ericsoni154@gmail.com>
2025-10-17 14:48:02 -0500

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,8 @@
+a = float(input("Ingrese el primer numero: "))
+b = float(input("INgrese el segundo numero: "))
+
+print("Suma: ", a+b)
+print("Resta: ", a-b)
+print("Multiplicacion", a*b)
+print("Division", a/b)
+print("Modulo: ", a%b)
