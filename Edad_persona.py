10c4717e7417e07f4fbf30d35b5592d2745d52de
manuxosos <ericsoni154@gmail.com>
2026-03-05 10:55:48 -0500

    2 ejercicios extra de sentencia IF

---
+++ Edad_persona.py
@@ -0,0 +1,6 @@
+edad = int(input("Ingrese su edad para saber si puede sufragar:"))
+
+if edad > 18 :
+    print ("Usted puede votar")
+else:
+    print("Usted no puede votar")
---
+++ mayor_50.py
@@ -0,0 +1,6 @@
+numero = int(input("Ingrese un numero:"))
+
+if numero > 50 :
+    print ("Su numero es mayor de 50:")
+else:
+    print ("Su numero es menor de 50:")
